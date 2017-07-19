163b5bbc1e35198e05c526515e3dde360dfe1b7c
Daniel.Newbury <Daniel.Newbury@Shopify.com>
2017-07-19 15:12:06 -0400

    Ch 9, iteration D3, playtime

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @count = increment_index_count
+  end
+
+  def increment_index_count
+    session[:counter].nil? ? session[:counter] = 1 : session[:counter] += 1
   end
 end
