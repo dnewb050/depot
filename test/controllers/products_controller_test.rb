95978bd4f9c1f97e0f64ce03ae7b1814e3c6b4a1
Daniel.Newbury <Daniel.Newbury@Shopify.com>
2017-07-19 13:05:05 -0400

    Ch 9, iteration D1/D2

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
   setup do
     @product = products(:one)
     @update = {
