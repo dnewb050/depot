807436d6d6074f4cbb3226505333c02c55d90322
Daniel.Newbury <Daniel.Newbury@Shopify.com>
2017-07-19 19:21:33 -0400

    Ch 10, iteration E3

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,8 +32,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart}
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -76,4 +75,5 @@ class LineItemsController < ApplicationController
     def line_item_params
       params.require(:line_item).permit(:product)
     end
+
 end
